b8784bd8b3e3cc1fdd33bbd2586f3c0114071d44
AbhijitGalange <abhijit.galange@gmail.com>
2020-03-07 14:52:28 +0530

    POC Test #3

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -4,9 +4,11 @@ public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
             System.out.println("I am covered");
+            System.out.println("This is Microsoft Azure DevOps CI CD POC done for ADT Project");
             return;
         }
 
         System.out.println("I am not covered");
+        System.out.println("This is Microsoft Azure DevOps CI CD POC done for ADT Project");
     }
 }
